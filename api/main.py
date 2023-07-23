2904b8fe46b5afa19883360ee625739b25dab759
crist16 <cristobalnegocio@gmail.com>
2023-07-23 19:40:39 -0400

    Commited3

--- api/main.py
+++ api/main.py
@@ -14,24 +14,25 @@ def Home():
 
 #Constancias
 
-@app.route('/constancia_de_trabajo', methods=['GET'])
+@app.route('/constancia_de_trabajo', methods=['POST'])
 def Constancia_de_trabajo():
+
     fecha_actual = getFullActualDate()
-    """
-     context = {
-        'Nombres' : request.form.get('Nombres'),
-        'Apellidos' : request.form.get('Apellidos'),
-        'Cedula' : request.form.get('Cedula'),
-        'Cargo' : request.form.get('Cargo'),
-        'fecha_ingreso' : request.form.get('Fecha_ingreso'),
-        'dia' : fecha_actual['dia'],
-        'mes' : fecha_actual['mes'],
-        'year' : fecha_actual['year'] ,
-        }
-        doc = DocxTemplate(r"inputs/templates/constancia_trabajo.docx")
-        doc.render(context)
-        doc.save(f"outputs/{request.form.get('Nombres')}.docx")
-    """
+
+    context = {
+    'Nombres' : request.form.get('Nombres'),
+    'Apellidos' : request.form.get('Apellidos'),
+    'Cedula' : request.form.get('Cedula'),
+    'Cargo' : request.form.get('Cargo'),
+    'fecha_ingreso' : request.form.get('Fecha_ingreso'),
+    'dia' : fecha_actual['dia'],
+    'mes' : fecha_actual['mes'],
+    'year' : fecha_actual['year'] ,
+    }
+    #doc = DocxTemplate(r"inputs/templates/constancia_trabajo.docx")
+    #doc.render(context)
+    #doc.save(f"outputs/{request.form.get('Nombres')}.docx")
+
     return 'received'
 
 @app.route('/constancia_de_estudio',methods =['POST'])
