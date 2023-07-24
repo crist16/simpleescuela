8581cce92afb8ae8a62b2eea2adee605898aa48e
crist16 <cristobalnegocio@gmail.com>
2023-07-24 12:03:10 -0400

    Commited6

--- api/main.py
+++ api/main.py
@@ -43,7 +43,7 @@ def Constancia_de_trabajo():
         doc.save(archivo_temporal)
 
         # Retornar el archivo temporal como descarga adjunta
-    return archivo_temporal
+    return send_file(archivo_temporal, as_attachment=True)
 
 
 
