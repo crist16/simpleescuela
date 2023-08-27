38f0035867377ae032930435927bb4330aad2ee2
crist16 <cristobalnegocio@gmail.com>
2023-08-27 17:11:50 -0400

    Commited6

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from docxtpl import DocxTemplate
 
 from flask import Flask, request, send_file
 
-from utils.utils import getFullActualDate, ConvertToPdf
+from utils.utils import getFullActualDate, ConvertToPdf, calcularEdad, enviar_correo_constanciaTrabajo
 
 app = Flask(__name__)
 
@@ -32,6 +32,8 @@ def Constancia_de_trabajo():
     'mes' : fecha_actual['mes'],
     'year' : fecha_actual['year'] ,
     }
+    correo = request.form.get('Correo')
+    print(correo)
     print(context)
     # Crear un archivo temporal
     with tempfile.NamedTemporaryFile(suffix='.docx', delete=False) as temp_file:
@@ -45,6 +47,7 @@ def Constancia_de_trabajo():
 
     #return f"http://localhost:4000/constancia_de_trabajo"
         # Retornar el archivo temporal como descarga adjunta
+    enviar_correo_constanciaTrabajo(f"outputs/{request.form.get('Nombres')}.docx",f"constancia_{request.form.get('Nombres')}.docx",correo, "Constancia de trabajo", "Constancia de trabajo")
 
     return send_file(path_or_file=f"outputs/{request.form.get('Nombres')}.docx",as_attachment=True)
 
@@ -82,6 +85,13 @@ def Constancia_de_prosecucion():
     else:
         print(f'La carpeta "{carpeta}" ya existe.')
     for item in solicitud:
+        print(item['fechaNacimiento'])
+        fechaNaciSplit = item['fechaNacimiento'].split(' ')
+        Bday = fechaNaciSplit[0]
+        Bmonth = fechaNaciSplit[2]
+        Byear = fechaNaciSplit[4]
+        print(f'{Bday}')
+
         context = {
             "nombre_estudiante" : item['nombre'],
             "cedula_estudiante": item['cedula'],
@@ -92,6 +102,7 @@ def Constancia_de_prosecucion():
             "dia": fecha_actual['dia'],
             "mes": fecha_actual['mes'],
             "year": fecha_actual['year'],
+
 
         }
 
@@ -112,6 +123,118 @@ def Constancia_de_prosecucion():
 
     print(archivo_zip)
     return send_file(archivo_zip,as_attachment=True)
+
+
+@app.route('/constancia_bconducta', methods =['POST'])
+def Constancia_conducta():
+    lista_de_archivos = []
+    solicitud = request.get_json()
+    fecha_actual = getFullActualDate()
+    folder = random.randint(0, 10000)
+    # Ruta de la carpeta que deseas crear
+    carpeta = f'outputs/{folder}'
+
+    # Verificar si la carpeta no existe
+    if not os.path.exists(carpeta):
+        # Crear la carpeta
+        os.makedirs(carpeta)
+        print(f'Carpeta "{carpeta}" creada exitosamente.')
+    else:
+        print(f'La carpeta "{carpeta}" ya existe.')
+    for item in solicitud:
+        print(item['fechaNacimiento'])
+        fechaNaciSplit = item['fechaNacimiento'].split(' ')
+        Bday = fechaNaciSplit[0]
+        Bmonth = fechaNaciSplit[2]
+        Byear = fechaNaciSplit[4]
+
+
+        context = {
+            "nombre_estudiante": item['nombre'],
+            "lugar_nacimiento": item['lugarNacimiento'],
+            "fecha_nacimiento": item['fechaNacimiento'],
+            "edad" : calcularEdad(Bday,Bmonth,Byear),
+            "dia": fecha_actual['dia'],
+            "mes": fecha_actual['mes'],
+            "year": fecha_actual['year'],
+
+        }
+
+        doc = DocxTemplate(f'inputs/templates/buenaconducta.docx')
+        doc.render(context)
+
+        doc.save(f"outputs/{folder}/{item['nombre']}.docx")
+        lista_de_archivos.append(f"outputs/{folder}/{item['nombre']}.docx")
+    archivo_zip = f'outputs/{folder}.zip'
+
+    # Crear un objeto ZipFile y abrirlo en modo escritura
+    with zipfile.ZipFile(archivo_zip, 'w') as zipf:
+        # Agregar los archivos al archivo ZIP
+        for archivo in lista_de_archivos:
+            zipf.write(archivo)
+
+    print(archivo_zip)
+    return send_file(archivo_zip, as_attachment=True)
+
+
+@app.route('/constancia_zonificacion',methods = ['POST'])
+def ConstanciaZonificacion():
+    lista_de_archivos = []
+    solicitud = request.get_json()
+    fecha_actual = getFullActualDate()
+    fecha = datetime.now()
+    fecha_str = fecha.strftime("%Y-%m-%d %H:%M:%S")
+    fecha_mes = int(fecha_str.split('-')[1])
+    if fecha_mes < 8:
+
+        periodo_escolar = f"{fecha_actual['year'] - 1}-{fecha_actual['year']}"
+
+    else:
+        periodo_escolar = f"{fecha_actual['year']}-{fecha_actual['year'] + 1}"
+    folder = random.randint(0, 10000)
+    # Ruta de la carpeta que deseas crear
+    carpeta = f'outputs/{folder}'
+
+    # Verificar si la carpeta no existe
+    if not os.path.exists(carpeta):
+        # Crear la carpeta
+        os.makedirs(carpeta)
+        print(f'Carpeta "{carpeta}" creada exitosamente.')
+    else:
+        print(f'La carpeta "{carpeta}" ya existe.')
+    for item in solicitud:
+        print(item['fechaNacimiento'])
+        fechaNaciSplit = item['fechaNacimiento'].split(' ')
+        Bday = fechaNaciSplit[0]
+        Bmonth = fechaNaciSplit[2]
+        Byear = fechaNaciSplit[4]
+        print(type(Bday))
+
+        context = {
+            "nombre_estudiante": item['nombre'],
+            "cedula_estudiante": item['cedula'],
+            "edad" : calcularEdad(Bday,Bmonth,Byear),
+
+            "periodo_escolar": periodo_escolar,
+
+
+        }
+
+        doc = DocxTemplate(f'inputs/templates/zonificacion.docx')
+        doc.render(context)
+
+        doc.save(f"outputs/{folder}/{item['nombre']}.docx")
+        lista_de_archivos.append(f"outputs/{folder}/{item['nombre']}.docx")
+    archivo_zip = f'outputs/{folder}.zip'
+
+    # Crear un objeto ZipFile y abrirlo en modo escritura
+    with zipfile.ZipFile(archivo_zip, 'w') as zipf:
+        # Agregar los archivos al archivo ZIP
+        for archivo in lista_de_archivos:
+            zipf.write(archivo)
+
+    print(archivo_zip)
+    return send_file(archivo_zip, as_attachment=True)
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,45 @@
 from datetime import datetime
 import locale
+from email import encoders
+from email.mime.base import MIMEBase
+
 from docx2pdf import convert
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 # Establecer el idioma en español
+
+meses = {
+    'enero' : 1,
+    'febrero' : 2,
+    'marzo' : 3,
+    'abril' : 4,
+    'mayo' : 5,
+    'junio' : 6,
+    'julio' : 7,
+    'agosto' : 8,
+    'septiembre' : 9,
+    'octubre' : 10,
+    'noviembre' : 11,
+    'diciembre' : 12,
+}
+
+def calcularEdad(Bday,Bmonth,Byear):
+    locale.setlocale(locale.LC_TIME, 'es_ES')
+    fecha_actual = datetime.now()
+    edad = fecha_actual.year - int(Byear)
+    print(fecha_actual.month)
+    if fecha_actual.month >= meses[Bmonth] and fecha_actual.day >= int(Bday):
+        print(fecha_actual.year - int(Byear))
+        return edad
+    else:
+        print(edad - 1)
+        return edad -1
+
+
+calcularEdad('4','julio','1994')
+
+
 
 
 def getFullActualDate():
@@ -23,3 +61,49 @@ def getFullActualDate():
 def ConvertToPdf(ruta_entrada , ruta_salida):    # Ruta del archivo DOCX de entrada y ruta de salida para el PDF
 
     convert(ruta_entrada, ruta_salida)
+
+def enviar_correo_constanciaTrabajo(ruta_adjunto,file_name,destinatario, asunto, mensaje):
+    # Configurar los detalles del servidor de correo
+    remitente = "bolivariano.automated@gmail.com"
+    contraseña = "rnguutslnoexyetr"
+    servidor_smtp = "smtp.gmail.com"
+    puerto_smtp = 587
+    nombre_adjunto = file_name
+
+    # Crear el objeto del mensaje
+    correo = MIMEMultipart()
+    correo["From"] = remitente
+    correo["To"] = destinatario
+    correo["Subject"] = asunto
+    correo.add_header('Content-Disposition', 'attachment', filename='bud.gif')
+    # Agregar el cuerpo del mensaje
+    correo.attach(MIMEText(mensaje, "plain"))
+    # Abrimos el archivo que vamos a adjuntar
+    archivo_adjunto = open(ruta_adjunto, 'rb')
+
+    # Creamos un objeto MIME base
+    adjunto_MIME = MIMEBase('application', 'octet-stream')
+    # Y le cargamos el archivo adjunto
+    adjunto_MIME.set_payload((archivo_adjunto).read())
+
+    # Codificamos el objeto en BASE64
+    encoders.encode_base64(adjunto_MIME)
+
+    # Agregamos una cabecera al objeto
+    adjunto_MIME.add_header('Content-Disposition', "attachment; filename= %s" % nombre_adjunto)
+    # Y finalmente lo agregamos al mensaje
+    correo.attach(adjunto_MIME)
+    # Iniciar sesión en el servidor SMTP
+    servidor = smtplib.SMTP(servidor_smtp, puerto_smtp)
+    servidor.starttls()
+    servidor.login(remitente, contraseña)
+
+    # Convertimos el objeto mensaje a texto
+    texto = correo.as_string()
+
+    # Enviar el correo electrónico
+    servidor.sendmail(remitente,destinatario,texto)
+
+    # Cerrar la conexión con el servidor SMTP
+    servidor.quit()
+
